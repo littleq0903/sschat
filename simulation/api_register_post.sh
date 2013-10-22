1a8e800b63d741b1d3dbe0e51e63008ada523c63
LittleQ <littleq0903@gmail.com>
2013-10-22 20:15:20 +0800

    for both compability

--- simulation/api_register_post.sh
+++ simulation/api_register_post.sh
@@ -1,3 +1,3 @@
 . ./server_env.sh
 
-curl -X POST "$SERVER_HOST/api/register" | sed -r "s/.*api_key\":\"(\w+)\".*/\1/g" 2> /dev/null
+curl --data "" "$SERVER_HOST/api/register" 2> /dev/null | sed -r "s/.*api_key\":\"(\w+)\".*/\1/g"
